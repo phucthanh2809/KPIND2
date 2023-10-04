45a6868d277203a38735571e18507ed6a2527ea8
Trần Bá Thiện <thienbadao1705@gmail.com>
2023-10-04 08:25:47 +0700

    Add project files.

---
+++ DuAn_QuanLyKPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using DuAn_QuanLyKPI.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuAn_QuanLyKPI
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Frm_Login());
+        }
+    }
+}
